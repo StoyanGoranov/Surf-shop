5f896bc26568a274705c23ef7da519297f9c924e
Stoyan <tony@tony.com>
2020-07-30 12:55:30 +0000

    Add user authentication

--- app.js
+++ app.js
@@ -2,14 +2,29 @@ const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
+const bodyParser = require ('body-parser');
 const logger = require('morgan');
+const session = require('express-session');
+const passport = require('passport');
+const User = require('./models/user');
+const mongoose = require("mongoose");
+//const passportLocalMongoose = require ('');
 
+//require routes
 const indexRouter = require('./routes/index');
 const postsRouter = require('./routes/posts');
 const reviewsRouter = require('./routes/reviews');
 const usersRouter = require('./routes/users');
 
 const app = express();
+
+//connect to DB
+mongoose.connect('mongodb://localhost:27017/surf-shop', {useNewUrlParser: true, useUnifiedTopology: true});
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));//maybe log the error?
+db.once('open', () => {
+  console.log("we're connected!");
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,6 +36,20 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//Configure  Passport and Sessions
+app.use(session({
+  secret: 'VeryLongAndUtterlyPointlessTextIsMySecret',
+  resave: false,
+  saveUninitialized: true,
+  //cookie: { secure: true }
+}))
+// CHANGE: USE "createStrategy" INSTEAD OF "authenticate"
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+//Mount routes
 app.use('/', indexRouter);
 app.use('/posts', postsRouter);
 app.use('/posts/:id/reviews', reviewsRouter);
